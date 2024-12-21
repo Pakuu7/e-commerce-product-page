b86ea61ff1203bceb82e26c69b423a609a3d888b
Pawel Dudziak <paweldudziak@spoko.pl>
2024-12-21 12:00:16 +0100

    Create gallery for desktop

--- src/js/main.js
+++ src/js/main.js
@@ -1,10 +1,10 @@
 const hamburgerNormalBtn = document.querySelector('.hamburger-normal')
 const hamburgerActiveBtn = document.querySelector('.hamburger-active')
 const menu = document.querySelector('.nav__items-mobile')
-const sliderContainer = document.querySelector('.main__slider-container')
+const sliderContainer = document.querySelector('.main__slider-mobile-container')
 const nextBtn = document.querySelector('.main__slider-btn--next')
 const prevBtn = document.querySelector('.main__slider-btn--prev')
-const slides = document.querySelectorAll('.main__slider-image')
+const slides = document.querySelectorAll('.main__slider-mobile-image')
 const quantityDecreaseBtn = document.querySelector('.product-details__quantity-btn--decrease')
 const quantityIncreaseBtn = document.querySelector('.product-details__quantity-btn--increase')
 const quantityValue = document.querySelector('.product-details__quantity-value')
@@ -15,30 +15,42 @@ const addToCartBtn = document.querySelector('.product-details__add-to-cart')
 const cartListProduct = document.querySelector('.nav__cart-list-product')
 const cartContainer = document.querySelector('.nav__cart-list-product')
 const cartListInfo = document.querySelector('.nav__cart-list-product-info')
+const desktopOtherImages = document.querySelectorAll('.main__gallery-desktop-image')
+const desktopMainImage = document.querySelector('.main__gallery-desktop-main-image')
 
 let currentIndex = 0
 let productAmountValue = 0
 
+function changeDesktopMainImage() {
+	desktopOtherImages.forEach(image => {
+		image.addEventListener('click', () => {
+			desktopMainImage.src = image.src
+		})
+	})
+}
+
+changeDesktopMainImage()
+
 addToCartBtn.addEventListener('click', () => {
-	if(productAmountValue > 0) {
+	if (productAmountValue > 0) {
 		const cartItem = document.createElement('div')
 		cartItem.className = 'nav__cart-item'
-	
+
 		const productImage = document.createElement('img')
 		productImage.src = 'dist/img/AirForce-1.png'
 		productImage.className = 'nav__cart-item-img'
 		productImage.alt = 'Product photo'
 		cartItem.appendChild(productImage)
-	
+
 		const productInfo = document.createElement('p')
 		productInfo.className = 'nav__cart-item-info'
 		productInfo.innerHTML = `
 		Air Force 1 Edition Sneakers $125.00 x 
 		<span class="nav__cart-item-pieces">${productAmountValue}</span> 
-		<span class="nav__cart-item-price-sum">$${(125.00 * productAmountValue).toFixed(2)}</span>
+		<span class="nav__cart-item-price-sum">$${(125.0 * productAmountValue).toFixed(2)}</span>
 		`
 		cartItem.appendChild(productInfo)
-	
+
 		const trashIcon = document.createElement('i')
 		trashIcon.className = 'nav__cart-item-icon fa-regular fa-trash-can'
 		trashIcon.addEventListener('click', () => {
@@ -46,8 +58,8 @@ addToCartBtn.addEventListener('click', () => {
 		})
 		cartItem.appendChild(trashIcon)
 		cartContainer.appendChild(cartItem)
-	
-		if(cartListInfo) {
+
+		if (cartListInfo) {
 			cartListInfo.remove()
 		}
 	}
